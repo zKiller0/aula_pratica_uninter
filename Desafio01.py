20f584adb9626c51850ec0a7aea91eead43ae021
Ito <derivaniasilva13364@gmail.com>
2023-09-14 13:57:03 -0300

    Primeira atualização 14/09/2023

---
+++ Desafio01.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+
+print('Bem vindo' , nome , 'é um prazer acompanhar você!' )
+             
---
+++ Desafio03.py
@@ -0,0 +1,3 @@
+n1 = inputNumber('Primeiro número : ')
+n2 = inputNumber('Segundo número : ')
+print(' A soma entre' , n1 , 'e' , n2 , 'é' , ( n1 + n2) )
